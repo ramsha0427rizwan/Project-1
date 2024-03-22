33ced862753c4bff2d1777bc25dd61a72885c226
ramsha0427 <rrizwan@hawk.iit.edu>
2024-03-21 23:48:34 -0500

    adding function validatemail

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 // jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
-    $('html').removeClass('nojs');
-    $('html').addClass('hasjs');
-  });
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});  
+
+  function ValidateEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
